534d9efa12ce61639d901bfa23c24e8c8ac81845
happy207508 <happy207508@naver.com>
2019-12-21 23:52:31 +0900

    프로젝트 최종파일

--- django/portfolio-project/animals/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Animal(models.Model):
-	image = models.ImageField(upload_to='images/')
-	Character = models.CharField(max_length=500)
